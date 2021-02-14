0f4c91674f12b21bfdc83d8adbf71ccc76d61b33
Aman Kumar <aman.kmr4@yahoo.com>
2021-02-14 15:31:17 +1100

    added styling to side bar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Item from './Item';
 import './style.css';
 
@@ -7,12 +6,6 @@ function Sidebar() {
     return (
         <aside>
             <ul className="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar">
-                <Link to="/" className="sidebar-brand d-flex align-items-center justify-content-center">
-                    <div className="sidebar-brand-icon rotate-n-15">
-                        <i className="far fa-user-circle" />
-                    </div>
-                    <div className="sidebar-brand-text mx-3">Company</div>
-                </Link>
 
                 {/* Divider */}
                 <hr className="sidebar-divider d-none d-md-block" />
