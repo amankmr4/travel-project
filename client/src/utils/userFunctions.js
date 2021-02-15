04027919d4d07eefee62d33f4c6f2a2d73f2fa9f
Aman Kumar <aman.kmr4@yahoo.com>
2021-02-15 23:27:04 +1100

    deleted Procfile

--- client/src/utils/userFunctions.js
+++ client/src/utils/userFunctions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const registerUser = (userData) => {
     // console.log(userData);
-    return axios.post('/api/register', {
+    return axios.post('http://localhost:3001/api/register', {
         first_name: userData.first_name,
         last_name: userData.last_name,
         email: userData.email,
@@ -12,12 +12,12 @@ export const registerUser = (userData) => {
 
 export const loginUser = (userData) => {
     // console.log(userData);
-    return axios.post('/api/login', {
+    return axios.post('http://localhost:3001/api/login', {
         email: userData.email,
         password: userData.password,
     });
 };
 
 export const getUsers = () => {
-    return axios.get('/api/displayusers').then((res) => res.data);
+    return axios.get('http://localhost:3001/api/displayusers').then((res) => res.data);
 };
